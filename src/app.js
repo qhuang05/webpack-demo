f9e7bfd4212ba6919f37b5ba49af9497e9a97596
qhuang05 <qhuang05@sina.com>
2019-07-29 07:15:19 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
-import a from './compontents/a'
-import b from './compontents/b'
-import c from './compontents/c'
+// import a from './compontents/a'
+// import b from './compontents/b'
+// import c from './compontents/c'
+// a()
+// b()
+// c()
 
-a()
-b()
-c()
+import React from 'react'
+import ReactDOM from 'react-dom'
+import imgBg from './images/bg001.jpg'
+import './style/common/common.css'
+import './style/common/font.css'
+import 'font-awesome/css/font-awesome.css'
+import StyleA from './style/a.css'
+
+ReactDOM.render(
+	<div>
+		<h3>Hello React</h3>
+		<img src={imgBg} />
+		<i className="icon-he-login"></i>
+		<i className="fa fa-envelope-open"></i>
+		<div className={StyleA .oa}>
+			<span className={StyleA.ob}>test</span>
+		</div>
+	</div>,
+	document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,66 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
 	entry: './src/app.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'main.js'
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			template: './src/index.html',		//以原有的文件为模板
+			filename: 'index.html',		//打包后输出的文件名,默认是index.html
+		})
+	],
+	module: {
+		rules: [{
+            test: /\.js$/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react']
+                }
+            }]
+        },{
+        	test: /\.(jpg|jpeg|png|gif)$/,
+        	use: [{
+        		loader:'url-loader',
+        		options:{
+        			limit:10000
+        		}
+        	}]
+        },{
+        	test: /\.(ttf|eot|woff|woff2|svg)$/,
+    		use: ['file-loader']
+        },
+        // {
+        // 	test: /\.css$/,
+        // 	use: ['style-loader', 'css-loader']
+        // },
+        {
+        	test: /\.css$/,
+        	use: ['style-loader', {
+        		loader: 'css-loader',
+        		options: {
+        			modules: true,
+        		}
+        	}],
+        	exclude: [
+        		path.resolve(__dirname, 'node_modules'),
+        		path.resolve(__dirname, 'src/style/common')
+        	]
+        },{
+        	test: /\.css$/,
+        	use: ['style-loader', 'css-loader'],
+        	include: [
+        		path.resolve(__dirname, 'node_modules'),
+        		path.resolve(__dirname, 'src/style/common')
+        	]
+        }
+        ]
+	},
+	devServer: {
+		open: true,
+		port: 8080
 	}
 }
